41162de6c562825e9432545ec957b8fb67aadd21
guillermodangelo <dangeloguillermo@gmail.com>
2023-03-12 12:38:02 -0300

    added new notebooks

--- raster_conversion.py
+++ raster_conversion.py
@@ -4,18 +4,19 @@ import os
 
 crs = QgsCoordinateReferenceSystem('EPSG:32721')
 qinst = QgsProject.instance()
-base_path = os.path.join(qinst.homePath())
+base_path = '/home/guillermo/Documents/GitHub/pool_detection/'
 
 
 files = os.listdir(os.path.join(base_path, 'images'))
 
 for file in files:
     if file[-3:] == 'jpg':
+        print(f'Processing file {file}')
         raster = file[:-4]
 
         processing.run("gdal:translate", {
             'INPUT': os.path.join(base_path, f'images/{raster}.jpg'),
             'SOURCE_CRS': crs,
             'TARGET_CRS': crs,
-            'OUTPUT': os.path.join(base_path, f'images/tiff/{raster}.tif')
+            'OUTPUT': os.path.join(base_path, 'images_tif', f'{raster}.tif')
             })
